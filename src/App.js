05cc6fe3617d03b9036fafa631c3a0a5fb539bcd
Igor M. <samplefines@gmail.com>
2025-02-26 16:32:39 +0300

    add Inter font for Headings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useSelector } from 'react-redux';
 import './App.css';
+import '@fontsource/inter'; // Defaults to weight 400
+import '@fontsource/inter/600.css'; // Specify weight
 import Description from './components/Description';
 import Main from './components/Main';
 import { selectShowDescription } from './redux/slices/showDescriptionSlice';
